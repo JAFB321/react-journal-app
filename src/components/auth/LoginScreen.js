5e860b792b40e7a5197b13969eb83cbab27bf99f
JAFB <jafb321@gmail.com>
2021-01-17 23:19:38 -0700

    Add login styles

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,29 +1,46 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export const LoginScreen = () => {
     return (
         <>
-          <h3>Login</h3>  
-          <form>
-              <input 
-                type="email"
-                placeholder="email"
-                name="email"
-              />
+            <h3 className="auth__title">Login</h3>
+            <form>
+                <input
+                    type="email"
+                    placeholder="email"
+                    name="email"
+                    className="auth__input"
+                    autoComplete="off"
+                />
+    
+                <input
+                    type="password"
+                    placeholder="password"
+                    name="password"
+                    className="auth__input"
+                />
 
-              <input 
-                type="password"
-                placeholder="password"
-                name="password"
-              />
-
-              <button type="submit">
-                Login
+                <button type="submit" className="btn btn-primary btn-block">
+                    Login
               </button>
+                <div className="auth__social-networks">
+                    <p>Login with social networks</p>
 
-              <hr/>
-              google
-          </form>
+                    <div
+                        className="google-btn"
+                    >
+                        <div className="google-icon-wrapper">
+                            <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+                        </div>
+                        <p className="btn-text">
+                            <b>Sign in with google</b>
+                        </p>
+                    </div>
+                </div>
+
+                <Link to="/auth/register">Create account</Link>
+            </form>
         </>
     )
 }
