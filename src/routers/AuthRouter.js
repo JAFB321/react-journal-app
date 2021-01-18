b2700e7806f180d1bdf23675ec150d5985755af7
JAFB <jafb321@gmail.com>
2021-01-17 22:34:37 -0700

    feat: Add auth screen styles

--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -4,21 +4,27 @@ import { LoginScreen } from '../components/auth/LoginScreen'
 import { RegisterScreen } from '../components/auth/RegisterScreen'
 
 export const AuthRouter = () => {
-    return (        
-        <Switch>
-            <Route
-                path="/auth/login/"
-                component={LoginScreen}
-                exact
-            />
+    return (
+        <div className="auth__main">
+            <div className="auth__box-container">
+                <Switch>
+                    <Route
+                        path="/auth/login/"
+                        component={LoginScreen}
+                        exact
+                    />
 
-            <Route
-                path="/auth/register/"
-                component={RegisterScreen}
-                exact
-            />
+                    <Route
+                        path="/auth/register/"
+                        component={RegisterScreen}
+                        exact
+                    />
 
-            <Redirect to="/auth/login" />
-        </Switch>
+                    <Redirect to="/auth/login" />
+                </Switch>
+            </div>
+
+        </div>
+
     )
 }
