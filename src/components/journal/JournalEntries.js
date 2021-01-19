d92ed3396c1c7b73d1b68960a020c2c3a98cddac
JAFB <jafb321@gmail.com>
2021-01-18 19:10:00 -0700

    feat: Add sidebar structure and styles

---
+++ src/components/journal/JournalEntries.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { JournalEntry } from './JournalEntry';
+
+export const JournalEntries = () => {
+    
+    const entries = [1,2,3,4,6,7,1,2,3,4,6,7,1,2,3,4,6,7];
+
+    return (
+        <div className="journal__entries">
+            {
+                entries.map(val => 
+                    <JournalEntry key={val} />
+                )
+            }
+        </div>
+    )
+}
--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Sidebar } from './Sidebar'
 
 export const JournalScreen = () => {
     return (
-        <div>
-            <h1 >Journal Main</h1>
+        <div className="journal__main-content">
+            
+        <Sidebar />
+
+        <main>
+            <h1>Main Content</h1>
+        </main>
         </div>
     )
 }
