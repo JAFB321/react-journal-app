efb789c71635b362f6c57ce61823d01835ee1e1e
JAFB321 <jafb321@gmail.com>
2021-06-01 12:28:41 -0700

    fix: repeated entries id's

--- src/components/journal/JournalEntries.js
+++ src/components/journal/JournalEntries.js
@@ -2,7 +2,7 @@ import React from "react";
 import { JournalEntry } from "./JournalEntry";
 
 export const JournalEntries = () => {
-	const entries = [1, 2, 3, 4, 6, 7, 1, 2, 3, 4, 6, 7, 1, 2, 3, 4, 6, 7];
+	const entries = [1, 2, 3, 4, 6, 7];
 
 	return (
 		<div className="journal__entries">
