0070dc6e0fc1b97cced05e68da7bbcf817bd9a9c
JAFB321 <jafb321@gmail.com>
2021-06-01 12:33:02 -0700

    feat: Add logout handle

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { startLogout } from "../../actions/auth";
 import { JournalEntries } from "./JournalEntries";
 
 export const Sidebar = () => {
+	const dispatch = useDispatch();
+
+	const handleLogout = () => {
+		dispatch(startLogout());
+	};
+
 	return (
 		<aside className="journal__sidebar">
 			<div className="journal__sidebar-navbar">
@@ -10,7 +18,9 @@ export const Sidebar = () => {
 					<span> Jose Antonio</span>
 				</h3>
 
-				<button className="btn">Logout</button>
+				<button onClick={handleLogout} className="btn">
+					Logout
+				</button>
 			</div>
 
 			<div className="journal__new-entry">
