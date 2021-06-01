9be70afc87704716ce734c877d16da2219b8b969
JAFB321 <jafb321@gmail.com>
2021-06-01 12:20:46 -0700

    feat: Add reducers action types

---
+++ src/types/types.js
@@ -0,0 +1,10 @@
+export const types = {
+	login: "[Auth] Login",
+	logout: "[Auth] Logout",
+
+	uiSetError: "[UI] setError",
+	uiRemoveError: "[UI] RemoveError",
+
+	uiStartLoading: "[UI] StartLoading",
+	uiStopLoading: "[UI] StopLoading",
+};
