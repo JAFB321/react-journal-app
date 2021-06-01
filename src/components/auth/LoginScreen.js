f80816bc0c1ed4ef4fabe02b670b7d6470f8768d
JAFB321 <jafb321@gmail.com>
2021-06-01 12:31:13 -0700

    feat: Add Google and email/psw auth

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,17 +1,46 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { startLoginEmailPassword, startGoogleLogin } from "../../actions/auth";
+import { useForm } from "../../hooks/useForm";
 
 export const LoginScreen = () => {
+	// Redux hooks
+	const dispatch = useDispatch();
+	const UIstate = useSelector((state) => state.ui);
+
+	const { loading } = UIstate;
+
+	const [formValues, handleInputChange] = useForm({
+		email: "tumamaypapa@gmail.ez",
+		password: "123tamarindo",
+	});
+
+	const handleLogin = (e) => {
+		e.preventDefault();
+
+		const { email, password } = formValues;
+		dispatch(startLoginEmailPassword(email, password));
+	};
+
+	const handleGoogleLogin = () => {
+		const { email, password } = formValues;
+		dispatch(startGoogleLogin());
+	};
+
 	return (
 		<>
 			<h3 className="auth__title">Login</h3>
-			<form>
+			<form onSubmit={handleLogin}>
 				<input
 					type="email"
 					placeholder="email"
 					name="email"
 					className="auth__input"
 					autoComplete="off"
+					value={formValues.email}
+					onChange={handleInputChange}
 				/>
 
 				<input
@@ -19,15 +48,17 @@ export const LoginScreen = () => {
 					placeholder="password"
 					name="password"
 					className="auth__input"
+					value={formValues.password}
+					onChange={handleInputChange}
 				/>
 
-				<button type="submit" className="btn btn-primary btn-block">
+				<button disabled={loading} type="submit" className="btn btn-primary btn-block">
 					Login
 				</button>
 				<div className="auth__social-networks">
 					<p>Login with social networks</p>
 
-					<div className="google-btn">
+					<div onClick={handleGoogleLogin} className="google-btn">
 						<div className="google-icon-wrapper">
 							<img
 								className="google-icon"
