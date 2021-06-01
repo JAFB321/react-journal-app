bbb76cef7d10db21fe13bfbd411de11e1a49200d
JAFB321 <jafb321@gmail.com>
2021-06-01 12:34:23 -0700

    feat: protect routes, listen firebase auth state and handle checking/loading states

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,16 +1,54 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import { firebase } from "../firebase/firebaseConfig";
+import { login } from "../actions/auth";
+
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 import { JournalScreen } from "../components/journal/JournalScreen";
 import { AuthRouter } from "./AuthRouter";
 
 export const AppRouter = () => {
+	const dispatch = useDispatch();
+
+	const [checking, setChecking] = useState(true);
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+	useEffect(() => {
+		firebase.auth().onAuthStateChanged((user) => {
+			if (user?.uid) {
+				const { uid, displayName } = user;
+				dispatch(login(uid, displayName));
+				setIsAuthenticated(true);
+			} else {
+				setIsAuthenticated(false);
+			}
+
+			setChecking(false);
+		});
+	}, [dispatch, setChecking]);
+
+	if (checking) {
+		return <h1>Espere...</h1>;
+	}
 	return (
 		<Router>
 			<div>
 				<Switch>
-					<Route path="/auth" component={AuthRouter} />
+					<PublicRoute
+						isAuthenticated={isAuthenticated}
+						path="/auth"
+						component={AuthRouter}
+					/>
 
-					<Route path="/" component={JournalScreen} exact />
+					<PrivateRoute
+						isAuthenticated={isAuthenticated}
+						path="/"
+						component={JournalScreen}
+						exact
+					/>
 
 					<Redirect to="/auth/login" />
 				</Switch>
